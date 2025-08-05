e446eef3dd6129357bdbd1ee89316307aa9d9147
ElinaGorniker <elinagorniker@gmail.com>
2025-08-05 10:24:00 +0200

    Improving guessing number game

--- PySamples/GuessNumber.py
+++ PySamples/GuessNumber.py
@@ -1,18 +1,44 @@
 import random
+import math
 
-def guessNumber():
-    a = random.randint(1,101)
+
+def is_valid(num):
+  if 1 <= int(num) <= 100:
+      return True
+  else:
+      return False
+
+def guess_number(a):
+    print("Welcome to the number guessing game!")
     n = 0
-    while n != a:
-        print("Your number:")
+    count = 0
+    while True:
+        print("Your number from 1 to 100:")
         n = int(input())
-        if n > a:
-            print("Too much, please try again")
-            continue
-        elif n < a:
-            print("Too low, please try again")
-            continue
+        if is_valid(n):
+            if n > a:
+                count += 1
+                print("Too much, please try again")
+                continue
+            elif n < a:
+                count += 1
+                print("Too low, please try again")
+                continue
+            else:
+                print("Congratulates! You win!")
+                break
         else:
-            print("Congratulates! You win!")
-            break
-guessNumber()
+            print("Or maybe let's enter an integer from 1 to 100?")
+            continue
+    attemts(a)
+    print ("Your attempts number: ", count)
+    print("Thank you for playing the number guessing game. See you again...")
+def attemts(a):
+    count = math.ceil(math.log2(a+1))
+    print("The minimum number of attempts required: ", count)
+
+a = random.randint(1,101)
+
+
+
+guess_number(a)
